e17d14a5d8d39db8934a27208411c9d8075699cd
= <tylermbayn@gmail.com>
2022-11-14 00:05:21 -0700

    Removed hard coded ghosts, ghost data is now stored in DB

---
+++ scripts/ghost_template.js
@@ -0,0 +1,32 @@
+class Ghost {
+    constructor(data){
+        this.ghostTemplate = `
+        <div class="ghost_card" id="${data.ghost}">
+                <div class="ghost_name">${data.ghost}</div>
+                <div class="ghost_speed">${this.toNumStr(data.min_speed)} m/s <span class="sound" onclick="toggleSound(${data.min_speed},'${data.ghost}0')">&#128266;</span>${data.max_speed == null ? '' : data.speed_is_range?' - ':' | '}${data.max_speed == null ? '' : this.toNumStr(data.max_speed)+' m/s <span class="sound" onclick="toggleSound('+data.max_speed+',\''+data.ghost+'1\')">&#128266;</span>'}${data.alt_speed == null ? '' : '<br>('+this.toNumStr(data.alt_speed)+' m/s <span class="sound" onclick="toggleSound('+data.alt_speed+',\''+data.ghost+'2\')">&#128266;</span>)'}</div>
+                <div class="ghost_hunt ${parseInt(data.hunt_sanity) > 50 ?'high':parseInt(data.hunt_sanity) < 50 ? 'low':'average'}">${data.hunt_sanity}</div>
+                <div class="ghost_evidence">${data.evidence[0]} | ${data.evidence[1]} | ${data.evidence[2]}</div>
+                <div class="ghost_behavior">
+                   ${data.behavior.map(this.behavior).join('<hr>')}
+                </div>
+                <div class="ghost_clear">
+                    <span class="check" onclick="select(this.parentElement.parentElement)"></span>
+                    <span class="space"></span>
+                    <span class="icon" onclick="fade(this.parentElement.parentElement)" ondblclick="remove(this.parentElement.parentElement)"></span>
+                </div>
+            </div>
+        `
+    }
+
+    behavior(value){
+        return '<div class="ghost_behavior_item">' + value + '</div>'
+    }
+
+    toNumStr(num) { 
+        if (Number.isInteger(num)) { 
+          return num + ".0"
+        } else {
+          return num.toString(); 
+        }
+      }
+}
