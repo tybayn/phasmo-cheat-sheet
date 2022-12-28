f91b7a048c76fecc0f0724e3b8762d53d60324ae
= <tylermbayn@gmail.com>
2022-12-28 14:40:53 -0700

    Altered footsteps to match at 150% and 50%, added metronome for sound

--- scripts/metronome.js
+++ scripts/metronome.js
@@ -1,17 +1,20 @@
 
-var snd = new Audio('assets/footstep.wav');
-snd.preload = 'auto';
-snd.load();
+var snd = [new Audio('assets/footstep.wav'),new Audio('assets/click.wav')];
+snd[0].preload = 'auto';
+snd[1].preload = 'auto';
+snd[0].load();
+snd[1].load();
 
 var speed = 1.7
 var tempo = 115
 var running = false
 var start = Date.now()
+var snd_choice = 0;
 
 var last_id = "";
 function toggleSound(set_tempo,id){
     speed = set_tempo
-    var speed_modifier = parseFloat($("#ghost_modifier_speed").val()) / 100
+    var speed_modifier = [0.48,0.75,1.00,1.29,1.62][parseInt($("#ghost_modifier_speed").val())]
     if (last_id != id){
         last_id = id
         tempo = ((9.6*Math.pow(set_tempo,2)) + (45.341*set_tempo) + 9.5862) * speed_modifier
@@ -32,21 +35,25 @@ function toggleSound(set_tempo,id){
     }
 }
 
+function setSoundType(){
+    snd_choice = document.getElementById("modifier_sound_type").checked ? 1 : 0;
+}
+
 function setTempo(){
-    var speed_modifier = parseFloat($("#ghost_modifier_speed").val()) / 100
+    var speed_modifier = [0.48,0.75,1.00,1.29,1.62][parseInt($("#ghost_modifier_speed").val())]
     tempo = ((9.6*Math.pow(speed,2)) + (45.341*speed) + 9.5862) * speed_modifier
 }
 
 function startMetronome() {
     running = true
     var interval = 1000 / (tempo / 60)
-    var footstep = snd.cloneNode()
+    var footstep = snd[snd_choice].cloneNode()
     footstep.play();
     setTimeout(step, interval);
     function step() {
         if (Date.now() - start <= 5000) {
             var interval = 1000 / (tempo / 60)
-            var footstep = snd.cloneNode()
+            var footstep = snd[snd_choice].cloneNode()
             footstep.play();
             setTimeout(step, interval);
         }
