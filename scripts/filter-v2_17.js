abc49ff4ee3ef3c9417e5bf51ddfb7c8da6fc5fd
= <tylermbayn@gmail.com>
2023-03-28 13:46:07 -0600

    Added guides tab, colored tabs, and a possible fix for discord tab not loading data

--- scripts/filter-v2_17.js
+++ scripts/filter-v2_17.js
@@ -615,6 +615,7 @@ function showSettings(){
         document.getElementById("settings_tab").style.boxShadow = "-6px 5px 5px -2px #000"
         document.getElementById("discord_link_box").style.zIndex= "1"
         document.getElementById("event_box").style.zIndex= "1"
+        document.getElementById("wiki_box").style.zIndex= "1"
         document.getElementById("settings_box").style.zIndex = "2"
         document.getElementById("settings_box").style.left = "196px"
     }
@@ -631,6 +632,7 @@ function showDiscordLink(){
         document.getElementById("discord_link_tab").style.boxShadow = "-6px 5px 5px -2px #000"
         document.getElementById("settings_box").style.zIndex = "1"
         document.getElementById("event_box").style.zIndex= "1"
+        document.getElementById("wiki_box").style.zIndex= "1"
         document.getElementById("discord_link_box").style.zIndex= "2"
         document.getElementById("discord_link_box").style.left = "196px"
     }
@@ -646,6 +648,7 @@ function showEvent(){
         document.getElementById("event_box").style.boxShadow = "5px 0px 10px 0px #000"
         document.getElementById("event_tab").style.boxShadow = "-6px 5px 5px -2px #000"
         document.getElementById("settings_box").style.zIndex = "1"
+        document.getElementById("wiki_box").style.zIndex= "1"
         document.getElementById("discord_link_box").style.zIndex= "1"
         document.getElementById("event_box").style.zIndex= "2"
         document.getElementById("event_box").style.left = "196px"
@@ -654,6 +657,23 @@ function showEvent(){
         document.getElementById("event_box").style.left = "-182px"
         document.getElementById("event_box").style.boxShadow = "none"
         document.getElementById("event_tab").style.boxShadow = "none"
+    }
+}
+
+function showWiki(){
+    if (document.getElementById("wiki_box").style.left == "-182px"){
+        document.getElementById("wiki_box").style.boxShadow = "5px 0px 10px 0px #000"
+        document.getElementById("wiki_tab").style.boxShadow = "-6px 5px 5px -2px #000"
+        document.getElementById("settings_box").style.zIndex = "1"
+        document.getElementById("discord_link_box").style.zIndex= "1"
+        document.getElementById("event_box").style.zIndex= "1"
+        document.getElementById("wiki_box").style.zIndex= "1"
+        document.getElementById("wiki_box").style.left = "196px"
+    }
+    else {
+        document.getElementById("wiki_box").style.left = "-182px"
+        document.getElementById("wiki_box").style.boxShadow = "none"
+        document.getElementById("wiki_tab").style.boxShadow = "none"
     }
 }
 
---
+++ wiki/wiki.js
@@ -0,0 +1,66 @@
+let ghost_flicker_data = {
+    "Normal":{
+        "vis_max":0.3,
+        "vis_min":0.08,
+        "invis_max":1.0,
+        "invis_min":0.3
+    },
+    "Phantom":{
+        "vis_max":0.3,
+        "vis_min":0.08,
+        "invis_max":2.0,
+        "invis_min":1.0
+    },
+    "Oni":{
+        "vis_max":1.0,
+        "vis_min":0.3,
+        "invis_max":0.3,
+        "invis_min":0.08
+    }
+}
+
+let flickering = false
+
+function startFlicker(elem){
+    let obj = $(elem).find("#ghost-flicker")
+    let ghost = $(elem).find("#flicker-ghost-name")[0].innerText
+    console.log(ghost)
+    let vis_min = ghost_flicker_data[ghost].vis_min
+    let vis_max = ghost_flicker_data[ghost].vis_max
+    let invis_min = ghost_flicker_data[ghost].invis_min
+    let invis_max = ghost_flicker_data[ghost].invis_max
+
+    function flickerOn(){
+        if (flickering){
+            $(obj).show()
+            r = Math.floor((Math.random() * (vis_max - vis_min) + vis_min) * 1000)
+            console.log(r)
+            setTimeout(flickerOff,r)
+        }
+    }
+
+    function flickerOff(){
+        if (flickering){
+            $(obj).hide()
+            r = Math.floor((Math.random() * (invis_max - invis_min) + invis_min) * 1000)
+            console.log(r)
+            setTimeout(flickerOn,r)
+        }
+    }
+
+    r = Math.floor((Math.random() * (vis_max - vis_min) + vis_min) * 1000)
+    $(obj).show()
+    flickering = true
+    setTimeout(flickerOff,r)
+}
+
+function setFlicker(){
+    if (flickering){
+        flickering=false
+    }
+    else{
+        startFlicker(document.getElementById("phantom-flicker"))
+        startFlicker(document.getElementById("normal-flicker"))
+        startFlicker(document.getElementById("oni-flicker"))
+    }
+}
