df87f8067d175763748148461557cb2d580b70d0
= <tylermbayn@gmail.com>
2023-09-09 15:37:39 -0600

    Fixed bpm finder to not mark ghosts on 0

--- scripts-v3/metronome-v2.js
+++ scripts-v3/metronome-v2.js
@@ -159,13 +159,13 @@ function get_ms(bpm){
         }
     })
 
-    return cur_ms.toFixed(2)
+    return bpm == 0 ? 0.00 : cur_ms.toFixed(2)
 }
 
 function get_ms_exact(bpm){
     var sm = [0.46,0.74,1.00,1.29,1.62][parseInt($("#ghost_modifier_speed").val())]
     var cur_ms = (Math.sqrt(38400000*(bpm / (sm*(1+(offset/100))))+1687696201)-45341)/(19200)
-    return cur_ms.toFixed(2)
+    return bpm == 0 ? 0.00 : cur_ms.toFixed(2)
 }
 
 function mark_ghosts(ms){
@@ -173,46 +173,49 @@ function mark_ghosts(ms){
     var ghosts = document.getElementsByClassName("ghost_card")
     for (var i = 0; i < ghosts.length; i++){
         ghosts[i].style.boxShadow = 'none'
-        var name = ghosts[i].getElementsByClassName("ghost_name")[0].textContent;
-        var speed = ghosts[i].getElementsByClassName("ghost_speed")[0].textContent
+        
+        if(ms != 0.00){
+            var name = ghosts[i].getElementsByClassName("ghost_name")[0].textContent;
+            var speed = ghosts[i].getElementsByClassName("ghost_speed")[0].textContent
 
-        //Parse Ghost speeds
-        if (speed.includes('|')){
-            var speeds = speed.split('|')
-            var speed_type = "or"
-        }
-        else if(speed.includes('-')){
-            var speeds = speed.split('-')
-            var speed_type = "range"
-        }
-        else{
-            var speeds = [speed]
-            var speed_type = "or"
-        }
+            //Parse Ghost speeds
+            if (speed.includes('|')){
+                var speeds = speed.split('|')
+                var speed_type = "or"
+            }
+            else if(speed.includes('-')){
+                var speeds = speed.split('-')
+                var speed_type = "range"
+            }
+            else{
+                var speeds = [speed]
+                var speed_type = "or"
+            }
 
-        // Get min and max
-        var min_speed = parseFloat(speeds[0].replaceAll(" m/s",""))
-        if (speeds.length > 1){
-            var max_speed = parseFloat(speeds[1].replaceAll(" m/s",""))
-        }
-        else{
-            var max_speed = min_speed
-        }
+            // Get min and max
+            var min_speed = parseFloat(speeds[0].replaceAll(" m/s",""))
+            if (speeds.length > 1){
+                var max_speed = parseFloat(speeds[1].replaceAll(" m/s",""))
+            }
+            else{
+                var max_speed = min_speed
+            }
 
-        if(document.getElementById("bpm_type").checked){
-            if ((speed_type == "range" && min_speed <= ms && ms <= max_speed) || name == "The Mimic"){
-                ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
+            if(document.getElementById("bpm_type").checked){
+                if ((speed_type == "range" && min_speed <= ms && ms <= max_speed) || name == "The Mimic"){
+                    ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
+                }
+                else if(min_speed === ms || max_speed === ms){
+                    ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
+                }
             }
-            else if(min_speed === ms || max_speed === ms){
-                ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
-            }
-        }
-        else{
-            if ((speed_type == "range" && (min_speed - 0.05) <= ms && ms <= (max_speed + 0.05)) || name == "The Mimic"){
-                ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
-            }
-            else if(((min_speed - 0.05) <= ms && ms <= (min_speed + 0.05)) || ((max_speed - 0.05) <= ms && ms <= (max_speed + 0.05))){
-                ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
+            else{
+                if ((speed_type == "range" && (min_speed - 0.05) <= ms && ms <= (max_speed + 0.05)) || name == "The Mimic"){
+                    ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
+                }
+                else if(((min_speed - 0.05) <= ms && ms <= (min_speed + 0.05)) || ((max_speed - 0.05) <= ms && ms <= (max_speed + 0.05))){
+                    ghosts[i].style.boxShadow = '0px 0px 10px 0px #dbd994'
+                }
             }
         }
     }
