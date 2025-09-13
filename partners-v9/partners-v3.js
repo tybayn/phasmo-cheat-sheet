5cbccaa7a3b5a659b5ee147c6e2bfd10fa6b3e9e
= <tylermbayn@gmail.com>
2025-09-13 12:23:07 -0600

    Twitch drops are displayed on partner tab

--- partners-v9/partners-v3.js
+++ partners-v9/partners-v3.js
@@ -12,4 +12,15 @@ $(window).on('load', function() {
     .catch(error => {
         // Om nom nom
     })
+
+    fetch("https://zero-network.net/zn/drops.html", {signal: AbortSignal.timeout(6000)})
+    .then(data => data.text())
+    .then(data => {
+        setTimeout(() => {
+            document.getElementById('drops_info_block').innerHTML += data
+        },1000);
+    })
+    .catch(error => {
+        // Om nom nom
+    })
 })
